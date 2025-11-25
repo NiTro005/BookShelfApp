80dfe696d63771388bbed1ca2329a4b82bfe1955
Kita Trofimov <144846094+NiTro005@users.noreply.github.com>
2025-11-25 15:05:51 +0300

    fix(BooksMainViewModel): Changed the data type of the books list in uiState

--- app/src/main/java/com/example/bookshelf/ui/BooksUiState.kt
+++ app/src/main/java/com/example/bookshelf/ui/BooksUiState.kt
@@ -1,10 +1,10 @@
-package com.example.bookshelf.ui.screens
+package com.example.bookshelf.ui
 
 import com.example.bookshelf.model.Book
 import com.example.bookshelf.model.ItemId
 
 data class BooksUiState(
-    val booksList: List<ItemId> = emptyList(),
+    val booksList: List<Book> = emptyList(),
     val currentBook: Book? = null,
     val status: LoadingStatus = LoadingStatus.Loading
 )
